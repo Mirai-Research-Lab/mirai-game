20234f9fc2031af012a2524d140ed5bb350b9651
Debajyoti <debajyotidhar111@gmail.com>
2022-11-05 20:59:50 +0530

    Added muzzle and shooting effects

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] private Transform t_origin;
+    [SerializeField] private Transform t_trailOrigin;
     [SerializeField] private float f_fireRate = 5f;
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private float maxDistance = 2000f;
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private ParticleSystem hitEffectVfx;
     private Ray ray;
     private RaycastHit hit;
     private float f_shootInterval = 0f;
@@ -25,8 +28,28 @@ public class PlayerShooting : MonoBehaviour
         ray = new Ray(t_origin.position, t_origin.forward);
 
         if (Physics.Raycast(ray, out hit, maxDistance))
-        { 
-
+        {
+            t_trailOrigin = GameObject.FindGameObjectWithTag("Muzzle").transform;
+            TrailRenderer trail = Instantiate(trailRenderer, t_trailOrigin.position, Quaternion.identity);
+            if(trail)
+                StartCoroutine(SpawnTrail(trail, hit));
         }
+    }
+
+    IEnumerator SpawnTrail(TrailRenderer trail, RaycastHit hit)
+    {
+        float time = 0;
+        Vector3 startPosition = trail.transform.position;
+
+        while(time < 0.5f)
+        {
+            trail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
+            time += Time.deltaTime / trail.time;
+
+            yield return null;
+        }
+        trail.transform.position = hit.point;
+        // Instantiate impact particle system
+        Instantiate(hitEffectVfx, hit.point, Quaternion.LookRotation(hit.normal));
     }
 }
